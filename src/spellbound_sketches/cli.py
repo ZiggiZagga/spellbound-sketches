9b9c1103e7845aff73b7dd2bfb7155e3b3932bc5
ZiggiZagga <75372480+ZiggiZagga@users.noreply.github.com>
2025-09-21 18:22:30 +0200

    Update cli.py

--- src/spellbound_sketches/cli.py
+++ src/spellbound_sketches/cli.py
@@ -1,1 +1,19 @@
+import logging
+import os
+import json
+import typer
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+name = "Spellbound Sketch CLI"
+logger = logging.getLogger(name)
+
+app = typer.Typer(pretty_exceptions_enable=False)
+
+@app.command()
+def run_sketch():
+    logger.info(f"run-sketch")
+
+
+if __name__ == "__main__":
+    app()
